84a1b0cbc7ad3c8e18f1c3a19785efd014fcb0ea
mayalin <mayalin2012@gmail.com>
2016-08-16 16:30:42 +0800

    add 10 admins/users/groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,19 @@
-puts "这个seed会自动建立1个admin账号, 和2个user账号"
+puts "这个seed会自动建立11个admin账号, 和10个user账号, 10个讨论组"
 
 create_account = User.create([email: 'admin@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
 puts "Admin account created."
 
-create_account = User.create([email: 'admin2@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
-puts "Admin2 account created."
+create_admin_accounts = for i in 1..10 do
+  User.create!([email: "admin#{i}@gmail.com", password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
+end
+  puts "10 admin_accounts created."
 
-create_account = User.create([email: 'user1@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'false'])
-puts "user1 account created."
+create_user_accounts = for i in 1..10 do
+    User.create!([email: "user#{i}@gmail.com", password: '12345678', password_confirmation: '12345678', is_admin: 'false'])
+end
+    puts "10 user_accounts created."
 
-create_account = User.create([email: 'user2@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'false'])
-puts "user2 account created."
+create_groups = for i in 1..10 do
+  Group.create!([title: "Group no.#{i}", description: "这是用seed建立的第 #{i} 个产品"])
+end
+  puts "10 Groups created."
