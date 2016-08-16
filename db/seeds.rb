060ac39084fe07bf437c0a97597ed69229f0044a
mayalin <mayalin2012@gmail.com>
2016-08-16 13:49:04 +0800

    add admin2 account to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,9 @@ puts "这个seed会自动建立1个admin账号, 和2个user账号"
 create_account = User.create([email: 'admin@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
 puts "Admin account created."
 
+create_account = User.create([email: 'admin2@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
+puts "Admin2 account created."
+
 create_account = User.create([email: 'user1@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'false'])
 puts "user1 account created."
 
