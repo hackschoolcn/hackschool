8bf1745ac46dc95851745479554ffa629aa5615d
chpwang <chpengwang@gmail.com>
2016-08-17 14:45:40 +0800

    add redcarpet to convert markdown to html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,25 @@ module ApplicationHelper
     end
   end
 
+  def markdown_to_html(text)
+    html_render_options = {
+      filter_html:     true, # no input tag or textarea
+      hard_wrap:       true,
+      link_attributes: { rel: 'nofollow' }
+    }
+
+    markdown_options = {
+      autolink:           true,
+      fenced_code_blocks: true,
+      lax_spacing:        true,
+      no_intra_emphasis:  true,
+      strikethrough:      true,
+      superscript:        true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(html_render_options)
+    markdown = Redcarpet::Markdown.new(renderer, markdown_options)
+    raw markdown.render(text)
+  end
+
 end
