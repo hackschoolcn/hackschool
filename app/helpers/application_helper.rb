d5870bc06f2bee5521bf4fc7bb4b0f2e494ad3a2
xdite <xuite.joke@gmail.com>
2016-08-18 11:27:12 +0800

    use class extend to markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,31 +1,9 @@
 module ApplicationHelper
-
   def render_formatted_time(time)
-    if time
-      time.to_formatted_s(:db) 
-    end
+    time.to_formatted_s(:db) if time
   end
 
   def markdown_to_html(text)
-    html_render_options = {
-      filter_html:     true, # no input tag or textarea
-      hard_wrap:       true,
-      link_attributes: { rel: 'nofollow' }
-    }
-
-    markdown_options = {
-      autolink:           true,
-      fenced_code_blocks: true,
-      lax_spacing:        true,
-      no_intra_emphasis:  true,
-      strikethrough:      true,
-      superscript:        true,
-      tables:             true
-    }
-
-    renderer = Redcarpet::Render::HTML.new(html_render_options)
-    markdown = Redcarpet::Markdown.new(renderer, markdown_options)
-    raw markdown.render(text)
+    text.to_markdown
   end
-
 end
