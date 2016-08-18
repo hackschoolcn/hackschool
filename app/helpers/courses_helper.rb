ae3af3cac70d1a9be9a37f49ed0c023aeff3c173
xdite <xuite.joke@gmail.com>
2016-08-18 11:34:41 +0800

    course teacher name

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,9 @@
 module CoursesHelper
+  def render_course_teacher_name(course)
+    course.teacher_name
+  end
+
+  def render_course_description(course)
+    course.description.to_markdown
+  end
 end
