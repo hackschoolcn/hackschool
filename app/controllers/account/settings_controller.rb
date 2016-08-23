b9c696c69455c0007a6f77d6ae805619d1ea6a63
Lanxinn <nlanxin@icloud.com>
2016-08-23 16:05:42 +0800

    fix no need table & admin/users account/settings

--- app/controllers/account/settings_controller.rb
+++ app/controllers/account/settings_controller.rb
@@ -2,16 +2,16 @@ class Account::SettingsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @setting = current_user
+    @user = current_user
   end
 
   def edit
-    @setting = current_user
+    @user = current_user
   end
 
   def update
-    @setting = current_user
-    if @setting.update(user_params)
+    @user = current_user
+    if @user.update(user_params)
       redirect_to account_setting_path, notice: "Update Success"
     else
       render :edit
@@ -21,6 +21,6 @@ class Account::SettingsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :nickname, :hobbies, :birthday, :sex, :selfintroduction, :address)
+    params.require(:user).permit(:username, :nickname, :hobbies, :birthday, :selfintroduction, :address, :gender)
   end
 end
