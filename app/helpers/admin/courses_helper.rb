25d64b8d81276371df7766f5f0d52cbf8493008c
xdite <xuite.joke@gmail.com>
2016-08-24 21:04:06 +0800

    refine admin index

--- app/helpers/admin/courses_helper.rb
+++ app/helpers/admin/courses_helper.rb
@@ -6,4 +6,20 @@ module Admin::CoursesHelper
       content_tag(:span, "", class: :"fa fa-globe")
     end
   end
+
+  def render_chapter_status(chapter)
+    if chapter.is_hidden
+      content_tag(:span, "", class: :"fa fa-lock")
+    else
+      content_tag(:span, "", class: :"fa fa-globe")
+    end
+  end
+
+  def render_post_status(post)
+    if post.is_hidden
+      content_tag(:span, "", class: :"fa fa-lock")
+    else
+      content_tag(:span, "", class: :"fa fa-globe")
+    end
+  end
 end
