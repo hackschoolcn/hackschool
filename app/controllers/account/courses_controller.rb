39e041875a6ff6d513dd73af31be2d4d19aa80e5
xdite <xuite.joke@gmail.com>
2016-08-24 21:32:52 +0800

    add breadcrumb

--- app/controllers/account/courses_controller.rb
+++ app/controllers/account/courses_controller.rb
@@ -48,6 +48,20 @@ class Account::CoursesController < AccountController
 
   protected
 
+  def set_breadcrumbs
+    @breadcrumbs = [view_context.link_to("My Account", "/account/courses")]
+  end
+
+  def drop_breadcrumb(title = nil, url = nil)
+    title ||= @page_title
+
+    if title && url
+      @breadcrumbs.push("<a href='#{url}'>#{title}</a>".html_safe)
+    elsif title
+      @breadcrumbs.push(title.to_s.html_safe)
+    end
+  end
+
   def validate_search_key
     @query_string = params[:query_string].gsub(/\\|\'|\/|\?/, "") if params[:query_string].present?
     @search_criteria = search_criteria(@query_string)
