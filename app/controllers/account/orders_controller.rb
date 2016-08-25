290945780fb3e999dd34d2caf9f144920dcfd6c1
chpwang <chpengwang@gmail.com>
2016-08-25 12:35:25 +0800

    resolve issue #117 - user can't purchase same course twice

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -8,32 +8,13 @@ class Account::OrdersController < AccountController
   end
 
   def yearly_subscription
-    @order = Order.new
-    @order.price = 6000
-    @order.subscription_months = 12
-    @order.user = current_user
-    @order.order_type = "subscription"
-    @order.save
-
-    flash[:notice] = "订单已创建"
-
-    redirect_to account_orders_path
+    create_order(months: 12, price: 6000, order_type: "subscription")
   end
 
   def yearly_subscription_from_course_view
     @course = Course.find(params[:course_id])
 
-    @order = Order.new
-    @order.price = 6000
-    @order.subscription_months = 12
-    @order.user = current_user
-    @order.course = @course
-    @order.order_type = "subscription"
-    @order.save
-
-    flash[:notice] = "订单已创建"
-
-    redirect_to account_orders_path
+    create_order(months: 12, price: 6000, order_type: "subscription", course: @course)
   end
 
   def single_purchase
@@ -43,21 +24,17 @@ class Account::OrdersController < AccountController
     if @course.is_hidden?  # check_course_valid
       redirect_to root_path, notice: "该课程没有开课"
     else
-      if current_user.member_of?(@course) # check encrolled_course uniq
-        redirect_to account_courses_path, warning: "您已参加课程中"
+      if current_user.is_valid_subscriber?
+        redirect_to course_path(@course), notice: "您是年费会员，可以免费报名，不必单独购买此课程"
       else
-        @order = Order.new
-        @order.price = @course.price
-        @order.user = current_user
-        @order.order_type = "single_purchase"
-        @order.course = @course
-        @order.save
-
-        flash[:notice] = "订单已创建"
-
-        redirect_to account_orders_path
+        if current_user.member_of?(@course) # check encrolled_course uniq
+          redirect_to account_courses_path, warning: "您已参加课程中"
+        else
+          create_order(months: 12, price: @course.price, order_type: "single_purchase", course: @course)
+        end
       end
-   end
+    end
+
   end
 
   def pay_with_wechat
@@ -109,4 +86,27 @@ class Account::OrdersController < AccountController
   def find_order
     @order = Order.find_by_token(params[:id])
   end
+
+
+  def create_order(options = {})
+    if current_user.orders.count > 0 && current_user.orders.last.unpaid?
+
+      flash[:warning] = "您有已创建的订单还未付款，可以继续操作付款，或取消订单重新选择"
+
+    else
+      @order = Order.new
+      @order.price = options[:price]
+      @order.order_type = options[:order_type]
+      @order.subscription_months = options[:months]
+      @order.course = options[:course]
+      @order.user = current_user
+      @order.save
+
+      flash[:notice] = "订单已创建"
+
+    end
+
+    redirect_to account_orders_path
+  end
+
 end
