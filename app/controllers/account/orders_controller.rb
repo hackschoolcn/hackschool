5accd4a6ccc690125204ff853381e5cceb8f2f75
xdite <xuite.joke@gmail.com>
2016-08-29 23:12:49 +0800

    use render layout

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -1,16 +1,16 @@
 class Account::OrdersController < AccountController
   before_action :find_order, only: %i(pay_with_wechat pay_with_alipay cancel_order)
   before_action :check_order_may_pay, only: %i(pay_with_wechat pay_with_alipay)
-  layout "order-details", only: [:show]
-  layout "user", only: [:index]
 
   def index
     @orders = current_user.orders.recent
     drop_breadcrumb "我的订单", account_orders_path
+    render layout: "user"
   end
 
   def show
     @order = Order.find(params[:id])
+    render layout: "order_details"
   end
 
   def yearly_subscription
