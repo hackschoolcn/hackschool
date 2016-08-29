0687d0d46d314a6b1bb29c05175c03f1c0fb5f9f
panxiubin <xiubinpan@gmail.com>
2016-08-29 17:35:51 +0800

    add geetest to sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "devise/users/registrations"}
 
   resources :courses do
     member do
