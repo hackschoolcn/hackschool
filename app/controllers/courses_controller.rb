d1bd62dc98408b2df43d1490a725e388d72940ab
mayalin <mayalin2012@gmail.com>
2016-09-01 13:45:52 +0800

    add course layout

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,7 @@
 class CoursesController < ApplicationController
   before_action :authenticate_user!, only: [:member_confirm_enroll, :join_favorite]
   before_action :check_enrolled_status, only: %i(enroll member_confirm_enroll)
-  #layout "course"
+  layout "course"
   layout "home-page", only:[:index]
 
   def index
@@ -55,7 +55,7 @@ class CoursesController < ApplicationController
       redirect_to root_path
       return
     end
-    
+
     if current_user.valid_subscriber?
       current_user.enroll_course!(@course)
       flash[:notice] = "报名成功"
