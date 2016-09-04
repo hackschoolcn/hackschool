7920db1d890617926099efd644469a4a669f7f39
mayalin <mayalin2012@gmail.com>
2016-09-04 11:19:21 +0800

    change action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'aqueous-savannah-80175.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'hackschool.herokuapp.com'}
+  #config.action_mailer.default_url_options = { :host => 'aqueous-savannah-80175.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
 
